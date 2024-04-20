0fc6fe802acfbe6e0399987e507b33c326460f85
peter dean <p.dean@internode.on.net>
2024-04-20 18:55:01 +1000

    perl

--- 2023/01/b.pl
+++ 2023/01/b.pl
@@ -1,18 +1,19 @@
 #!/usr/bin/env perl
 use v5.10;
 
-my @words = qw( one two three four five six seven eight nine);
+my @words  = qw( one two three four five six seven eight nine);
 my @digits = (1..9);
-my @keys = (@digits,@words);
-my @vals = (@digits,@digits);
-my %hash = map {($keys[$_],$vals[$_])} (0 .. $#keys);
-say "@{[%hash]}";
-my $re = join '|',@keys;
-my $sum = 0;
+my @keys   = (@digits,@words);
+my @vals   = (@digits,@digits);
+my %dict   = map {($keys[$_],$vals[$_])} (0 .. $#keys);
+my $re     = join '|',@keys;
+my $sum    = 0;
+
+say "@{[%dict]}";
 
 while (<>) {
     my ($d1) = /  ($re)/x;
     my ($d2) = /.*($re)/x;
-    $sum += "$hash{$d1}$hash{$d2}";
+    $sum += "$dict{$d1}$dict{$d2}";
 }
 say $sum;
