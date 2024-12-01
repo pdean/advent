25a2751d4bcb029173aec267d20c160826e80380
peter dean <p.dean@internode.on.net>
2024-12-01 15:48:37 +1000

    no comment

--- 2023/13/sol.pl
+++ 2023/13/sol.pl
@@ -8,6 +8,40 @@ use v5.38;
 my $solution_1 = 0;
 my $solution_2 = 0;
 
+$/ = "";
+
+while (<>) {
+    my @rows = split;
+    my $m = @rows;  # number of rows
+    my $n = length $rows[0]; #number of columns
+    say "@rows";
+    say "$m rows $n columns";
+
+    my @lrows;
+    for my $row (@rows) {
+        push @lrows, [split "", $row];
+    }
+    my @lcols;
+    for my $row (@lrows) {
+        for my $col (0 .. $#{$row}) {
+            push(@{$lcols[$col]}, $row->[$col]);
+        }
+    }
+    my @cols;
+    for my $col (@lcols) {
+        push @cols, "@$col";
+    }
+    for my $col (@cols) {
+        say "$col";
+    }
+}
+
+
+
+
+
+
+
 say "Solution 1: $solution_1";
 say "Solution 2: $solution_2";
 
--- 2024/01/sol.pl
+++ 2024/01/sol.pl
@@ -2,11 +2,38 @@
 # vim:ft=perl:sts=4:sw=4:et
 
 use v5.38;
+use List::Util qw[mesh];
 
 @ARGV = "input" unless @ARGV;
 
 my $solution_1 = 0;
 my $solution_2 = 0;
+
+my ( @l, @r );
+
+while (<>) {
+    chomp;
+    my ( $l, $r ) = split;
+    push @l, $l;
+    push @r, $r;
+}
+
+@l = sort { $a <=> $b } @l;
+@r = sort { $a <=> $b } @r;
+
+for my ( $l, $r ) ( mesh \@l, \@r ) {
+    $solution_1 += abs( $r - $l );
+}
+
+my %t;
+for my $r (@r) {
+    $t{$r}++;
+}
+
+for my $l (@l) {
+    my $n = $t{$l} // 0;
+    $solution_2 += $l * $n;
+}
 
 say "Solution 1: $solution_1";
 say "Solution 2: $solution_2";
